0c23fb763a1dbd6d1967ab90ea484fd95f6cca41
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-28 10:14:01 -0600

    moving click handler

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,15 @@ export default function Card({ item, index, indexExpanded, onExpand }) {
   const isExpanded = index === indexExpanded;
   const cardNumber = index + 1;
   return (
-    <div className={isExpanded ? "item open" : "item"}>
+    <div
+      className={isExpanded ? "item open" : "item"}
+      onClick={() => onExpand(index)}
+    >
       <h1 className="number">
         {cardNumber < 10 ? "0" + cardNumber : cardNumber}
       </h1>
       <h1 className="title">{item.title}</h1>
-      <span className="icon" onClick={() => onExpand(index)}>
+      <span className="icon">
         {isExpanded ? <strong>-</strong> : <strong>+</strong>}
       </span>
       <br />
