0194750ee3991909210e1422ac692ada087afe5b
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-28 09:48:13 -0600

    add conditional formatting for card numbers

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
   const [indexExpanded, setIndexExpanded] = useState(0);
 
   function handleIndexExpansion(index) {
-    console.log(index);
     setIndexExpanded(index);
   }
   return (
@@ -35,17 +34,19 @@ function App() {
 }
 
 function Card({ item, index, indexExpanded, onExpand }) {
+  const isExpanded = index === indexExpanded;
+  const cardNumber = index + 1;
   return (
-    <div className="">
-      <div className="item">
-        <h1 className="number">{index + 1}</h1>
-        <h1 className="title">{item.title}</h1>
-        <span className="icon" onClick={() => onExpand(index)}>
-          <strong>+</strong>
-        </span>
-        <br />
-        {indexExpanded === index ? <p>{item.text}</p> : <></>}
-      </div>
+    <div className={isExpanded ? "item open" : "item"}>
+      <h1 className="number">
+        {cardNumber < 10 ? "0" + cardNumber : cardNumber}
+      </h1>
+      <h1 className="title">{item.title}</h1>
+      <span className="icon" onClick={() => onExpand(index)}>
+        <strong>+</strong>
+      </span>
+      <br />
+      {isExpanded ? <p>{item.text}</p> : <></>}
     </div>
   );
 }
